020bec32a1b36a717ca028b46fe0ba639ec5b552
unknown <jamestomale@gmail.com>
2018-10-10 21:06:38 -0400

    update

---
+++ easyrider/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => (
+  <div>
+    <h1>EASY RIDER</h1>
+    <p>
+      BLAH BLAH BLAH TEXT BLAH BLAH BLAH LOREM IPSUM SHIIZ.
+    </p>
+  </div>
+);
+
+export default Home;

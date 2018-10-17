7dcf98bbf75e5ab5d2306ff7ea78af3268ca9f41
plsaladi1013 <plsaladi1013@gmail.com>
2018-10-17 01:46:21 -0400

    Pushpa adding the Comparison component to the master

--- easyrider/src/pages/Home.js
+++ easyrider/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+import RideComparison from './RideComparison';
 const Home = () => (
   <div>
     <h1>EASY RIDER</h1>
-    <p>
+    {/* <p>
       BLAH BLAH BLAH TEXT BLAH BLAH BLAH LOREM IPSUM SHIIZ.
-    </p>
+    </p> */}
+    <RideComparison />
   </div>
 );
 
